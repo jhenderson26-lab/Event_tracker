43bc43d452e109124304b6ad1d55dac20c88750a
jhenderson26-lab <jhenderson26@basecampcodingacademy.org>
2025-12-03 15:10:48 -0600

    backend done

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', home , name='home'),
     path('Login&Signup/signup', register_view, name='signup'),
     path('Login&Signup/login', login_view, name='login'),
-    path('create/', event_view, name='events'),]
+    path('events/', event_view, name='events'),
+    path('create_event/', create_view, name='create_event'),
+    path('delete_event/<event_id>', delete_event, name="delete")]
--- app/views.py
+++ app/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from datetime import timedelta, datetime
+from datetime import date
+from . import models
+from . import forms
+
 # Create your views here.
+def getting_user(request):
+    user_id = request.user.id
+    owner = models.User.objects.get(id=user_id)
+    return owner
 
 def home(request):
-    return render(request, 'base.html')
+    if request.user.is_authenticated:
+        owner = getting_user(request)
+        context = {'owner': owner}
+    else:
+        context = None
+    return render(request, 'base.html', context)
 
 def register_view(request):
     if request.method == 'POST':
@@ -30,7 +44,53 @@ def login_view(request):
     context = { 'form': form }
     return render(request, 'Login&Signup/login.html', context)
 
-@login_required
+def check_event(day):
+    int_date = int((f'{day.event_date.year}' + f'{day.event_date.month}' + f'{day.event_date.day}'))
+    tday = date.today()
+    int_today = int((f'{tday.year}' + f'{tday.month}' + f'{tday.day}'))
+    if int_date > int_today:
+        day.save()
+    else:
+        day.delete()
+
+
 def event_view(request):
-    return render(request, 'ceate.html')
+    if request.user.is_authenticated:
+        event = forms.EventForm()
+        ower = getting_user(request)
+        event_list = models.Event.objects.filter(user=ower)
+        list_order = models.Event.objects.filter(user=ower).order_by("event_date")
+        for events in event_list:
+            if events.until_event == None:
+                events.delete()
+            check_event(events)
+        context = {'event': event, 'text': list_order}
+    else:
+        context = {'text': 'Sorry, you need to login in.'}
+    return render(request, 'events.html', context)
+
+
+@login_required
+def create_view(request):
+    if request.method == 'POST':
+        ower = getting_user(request)
+        form = forms.EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.user = ower
+            event.save()
+
+           
+            for day in models.Event.objects.filter(user=ower):
+                day.event_date = day.created_at.date() + timedelta(days=day.set_date)
+                day.save()
+                check_event(day)
+
+    return redirect('events')
+    
+
+def delete_event(request, event_id):
+    event = models.Event.objects.get(id=event_id)
+    event.delete()
+    return redirect('events')
 
